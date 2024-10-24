dccd61438ea9f24ba8f74315c2dbae81b3fda6a9
Laer-tty <emaildolaertty@gmail.com>
2024-10-23 23:02:54 -0300

    Initial commit

---
+++ Projeto-API-REST-Claro-Spring-Boot/src/main/java/me/dio/claro_spring_boot/Aplication.java
@@ -0,0 +1,14 @@
+package me.dio.claro_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aplication.class, args);
+
+	}
+
+}
---
+++ Projeto-API-REST-Claro-Spring-Boot/src/test/java/me/dio/claro_spring_boot/ClaroSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.claro_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaroSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
